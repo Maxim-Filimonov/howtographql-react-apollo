0ea9098ca9f67212041ec88c68b421ec5fa98c6a
Maxim Filimonov <tpaktopsp@gmail.com>
2018-03-13 12:49:48 +0800

    cleanup index

--- src/index.js
+++ src/index.js
@@ -7,29 +7,11 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import gql from "graphql-tag";
 
 const link = new HttpLink({ uri: "http://localhost:4000" });
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({ link, cache });
-
-client
-  .query({
-    query: gql`
-      query FeedQuery {
-        feed {
-          links {
-            id
-            createdAt
-            description
-            url
-          }
-        }
-      }
-    `
-  })
-  .then(res => console.log("GQL:", res.data.feed));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
